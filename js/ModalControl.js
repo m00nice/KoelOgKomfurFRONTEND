afe77d9b8f9044bde816019e2671a2a9235be0d4
augu0577 <august.hawk@gmail.com>
2022-12-06 09:42:13 +0100

    Added working buttons to each product

--- js/ModalControl.js
+++ js/ModalControl.js
@@ -2,7 +2,10 @@
  var modal = document.getElementById("myModal");
       
  // Get the button that opens the modal
- var btn = document.getElementById("modalBtn");
+ var btn1 = document.getElementById("modalBtn1");
+ var btn2 = document.getElementById("modalBtn2");
+ var btn3 = document.getElementById("modalBtn3");
+ var btn4 = document.getElementById("modalBtn4");
  
  // Get the <span> element that closes the modal
 // var span = document.getElementsByClassName("close")[0];
@@ -12,9 +15,18 @@ function closePopup() {
 }
  
  // When the user clicks the button, open the modal 
- btn.onclick = function() {
+ btn1.onclick = function() {
    modal.style.display = "block";
  }
+ btn2.onclick = function() {
+  modal.style.display = "block";
+}
+btn3.onclick = function() {
+  modal.style.display = "block";
+}
+btn4.onclick = function() {
+  modal.style.display = "block";
+}
  
  // When the user clicks on <span> (x), close the modal
  // span.onclick = function() {

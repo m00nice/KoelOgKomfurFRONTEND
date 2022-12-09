21c9ee5c321357ae19d270dcfa8604ebabb589d6
m00ni <m00nice@live.com>
2022-12-09 14:08:23 +0100

    generate product 2

--- js/GenerateProducts.js
+++ js/GenerateProducts.js
@@ -1,3 +1,6 @@
+var modal = document.getElementById("content-modal");
+
+
 class GenerateProducts{
 
 
@@ -24,8 +27,33 @@ class GenerateProducts{
         this.updatePage();
     
     }
+    
 
     updatePage(){
+
+      var modal = document.getElementById("content-modal");
+
+      let modalHTML = `<div class="modal-content">
+      <div class="close"><a id="closeModal" href="javascript:closePopup()">&times;</a></div>
+      <div class="row">
+        <div id="modal-img" class="col-5">
+         
+        </div>
+        <div class="col">
+          <h1 id="modal-titel">Titel</h1>
+            <h2 id="modal-price">price</h2>
+            <p><small>Told og moms inkluderet<a
+                  href="https://kologkomfur.dk/policies/shipping-policy">Leveringskomkostninger</a> udregnet ved
+                betaling.</small></p>
+                <hr>
+            <p class="productInfo">
+              
+            </p>
+        </div>
+      </div>
+    </div>`;
+
+      modal.innerHTML = modalHTML;
 
         function alphAsc(a, b) {
             const nameA = a.name;
@@ -61,7 +89,7 @@ class GenerateProducts{
         
 
         <div class="col-4 my-2">
-        <div class="card" id="prod-${productList[i].id}">
+        <div class="card" id="${productList[i].id}">
           <img
             class="card-img-top"
             src="${productList[i].imageURL}"
@@ -72,7 +100,7 @@ class GenerateProducts{
             <span class="card-text">DOBBELT KOMFUR - 90 CM</span>
             <p class="card-text">${productList[i].brand.name}</p>
             <p class="card-texta">fra ${productList[i].price} kr.</p>
-            <a id="modalBtn" class="btn btn-primary">Se produkt</a>
+            <button id="modalBtn-${productList[i].id}" onclick='fillModal(${JSON.stringify(productList[i])})' class="btn btn-primary">Se produkt</button>
             
           </div>
         </div>
@@ -87,9 +115,49 @@ class GenerateProducts{
         htmlCode = divStart + htmlCode + divEnd;
 
         div.innerHTML = htmlCode;
+      
 
-    ;
+    
 
 }
+
 }
+
+ //fill modal with product's values
+function fillModal(producti){
+            
+            var modal = document.getElementById("content-modal");
+
+            console.log(producti);
+            producti = JSON.stringify(producti)
+            producti = JSON.parse(producti);
+
+            console.log(producti);
+
+            var modal = document.getElementsByClassName("modal");
+            var titel = document.getElementById("modal-titel");
+            var price = document.getElementById("modal-price");
+            var img = document.getElementById("modal-img");
+            var info = document.getElementById("productInfo");
+            
+            if(producti.name != null){titel.innerHTML = producti.name;}
+            
+            if(producti.price != null){price.innerHTML = producti.price;}
+
+            if(producti.imageURL != null && producti.name != null){img.innerHTML = `<img class="card-img-modal" src="${producti.imgURL}" alt="${producti.name} IMAGE" />`;}
+            
+            if(producti.info != null){info.innerHTML = producti.info;}
+
+            modal.display = "block";
+          }
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.display = "none";
+  }
+}
+
+
 var generateProducts = new GenerateProducts();
+
+var productList  =  generateProducts.dataProduct;
--- js/main.js
+++ js/main.js
@@ -52,43 +52,4 @@ loadCartNumber();
 
 
 
-// Get the modal
 
-let modal = document.querySelectorAll(".modal");
-     
-
-// Get the button that opens the modal
-
-
-var btn = document.querySelectorAll(".modalBtn");
-
-
-
-function closePopup() {
-
- modal.style.display = "none";
-
-}
-
-
-
-// When the user clicks the button, open the modal
-
-btn.onclick = function() {
-
-  modal.style.display = "block";
-
-}
-
-
-// When the user clicks anywhere outside of the modal, close it
-
-window.onclick = function(event) {
-
-  if (event.target == modal) {
-
-    modal.style.display = "none";
-
-  }
-
-}

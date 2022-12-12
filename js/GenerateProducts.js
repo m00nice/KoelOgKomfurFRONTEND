487167e020f507201b9d87e4d69d6c24040a6d33
m00ni <m00nice@live.com>
2022-12-12 02:07:04 +0100

    commit

--- js/GenerateProducts.js
+++ js/GenerateProducts.js
@@ -1,13 +1,10 @@
-var modal = document.getElementById("content-modal");
+var modalContent = document.getElementById("content-modal");
 
+var cart = document.getElementById("cart")
 
 class GenerateProducts{
-
-
+ 
     endpointUrlProduct = "http://localhost:8080/product";
-
-    
-    
 
     constructor(dataProduct){
         this.dataProduct = dataProduct;
@@ -31,29 +28,29 @@ class GenerateProducts{
 
     updatePage(){
 
-      var modal = document.getElementById("content-modal");
-
       let modalHTML = `<div class="modal-content">
       <div class="close"><a id="closeModal" href="javascript:closePopup()">&times;</a></div>
       <div class="row">
+      <div id="modal-id" style="display: none;"></div>
         <div id="modal-img" class="col-5">
          
         </div>
         <div class="col">
           <h1 id="modal-titel">Titel</h1>
             <h2 id="modal-price">price</h2>
+            <h3><button id="add-to-cart" onclick="">Tilføj til kurv</button></h3>
             <p><small>Told og moms inkluderet<a
-                  href="https://kologkomfur.dk/policies/shipping-policy">Leveringskomkostninger</a> udregnet ved
+                  href="https://kologkomfur.dk/policies/shipping-policy" target="_blank">Leveringskomkostninger</a> udregnet ved
                 betaling.</small></p>
                 <hr>
-            <p class="productInfo">
+            <p id="productInfo">
               
             </p>
         </div>
       </div>
     </div>`;
 
-      modal.innerHTML = modalHTML;
+      modalContent.innerHTML = modalHTML;
 
         function alphAsc(a, b) {
             const nameA = a.name;
@@ -117,6 +114,15 @@ class GenerateProducts{
         div.innerHTML = htmlCode;
       
 
+        let addToCartButton = document.getElementById("add-to-cart");
+
+        console.log(addToCartButton);
+        
+        
+            addToCartButton.addEventListener('click', () => {
+                addProductToCart();
+                cartAmount();
+            })
     
 
 }
@@ -126,7 +132,7 @@ class GenerateProducts{
  //fill modal with product's values
 function fillModal(producti){
             
-            var modal = document.getElementById("content-modal");
+            
 
             console.log(producti);
             producti = JSON.stringify(producti)
@@ -134,11 +140,13 @@ function fillModal(producti){
 
             console.log(producti);
 
-            var modal = document.getElementsByClassName("modal");
+            var idM = document.getElementById("modal-id");
             var titel = document.getElementById("modal-titel");
             var price = document.getElementById("modal-price");
             var img = document.getElementById("modal-img");
             var info = document.getElementById("productInfo");
+
+            if(producti != null){idM.innerHTML = JSON.stringify(producti);}
             
             if(producti.name != null){titel.innerHTML = producti.name;}
             
@@ -146,18 +154,152 @@ function fillModal(producti){
 
             if(producti.imageURL != null && producti.name != null){img.innerHTML = `<img class="card-img-modal" src="${producti.imgURL}" alt="${producti.name} IMAGE" />`;}
             
-            if(producti.info != null){info.innerHTML = producti.info;}
-
-            modal.display = "block";
+            if(producti.description != null){info.innerHTML = producti.description;}
+            console.log(modalContent.style.display);
+            modalContent.style.display = "block";
           }
 
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.display = "none";
+  
+  if (event.target == modalContent) {
+    modalContent.style.display = "none";
   }
+
+  if(event.target == cart){
+    cart.style.display = "none";
+  }
+
 }
 
 
 var generateProducts = new GenerateProducts();
 
-var productList  =  generateProducts.dataProduct;
+
+
+
+
+  
+
+//CART FUCTIONS
+function loadCartNumber(){
+  let productNumber = localStorage.getItem('cartNumber')
+
+  if(productNumber){
+      document.getElementById('cartNumber').textContent = productNumber;
+  }
+}
+
+function cartAmount() {
+
+  let productNumber = localStorage.getItem('cartNumber');
+
+  productNumber = parseInt(productNumber);
+
+  if(productNumber){
+      localStorage.setItem('cartNumber', productNumber + 1)
+      document.getElementById('cartNumber').textContent = productNumber +1;
+  }else{
+      localStorage.setItem('cartNumber', 1)
+      document.getElementById('cartNumber').textContent = 1;
+  }
+
+  
+
+}
+
+function addProductToCart(){
+  let cartItems = localStorage.getItem('productsInCart');
+  cartItems = JSON.parse(cartItems);
+  console.log("items in cart",cartItems);
+    
+  let grId = () => {return Math.floor((1 + Math.random())* 0x10000).toString(16).substring(1)};
+
+  let rId = grId()+'-'+grId()+'-'+grId()+'-'+grId();
+
+  let frId = "arraySplit"+rId+"-"+rId+"-"+rId+"-"+rId+"arraySplit";
+
+  var product = document.getElementById("modal-id").textContent;
+
+  product = JSON.parse(product);
+
+  if(cartItems != null){
+    if(cartItems[product] == undefined){
+      cartItems = {
+      ...cartItems,
+      [frId]: product
+    }}
+  }else{
+    cartItems = {
+  [frId] : product
+ }}
+
+  
+
+  localStorage.setItem("productsInCart", JSON.stringify(cartItems))
+  
+
+}
+
+function showCart(){
+  cart = document.getElementById("cart");
+  var itemId = localStorage.getItem("productsInCart").slice(0,-1)
+  console.log(itemId);
+  var itemId = itemId.split("arraySplit\":").join("arraySplit").split("arraySplit");
+  itemId.splice(0,1)
+  itemId.pop;
+  var cartItems = [];
+  console.log(itemId);
+  for (var i = itemId.length - 1; i >= 0; i--) {
+    // remove element if index is odd
+    if (i % 2 == 1)
+      cartItems.push(itemId[i, 1]);
+  }
+  console.log(cartItems);
+
+  for(var i = 0; i < cartItems.length; i++){
+    var sliceItem = cartItems[i].slice(0 ,cartItems[i].length-2)
+    cartItems[i] = sliceItem.slice(0 ,cartItems[i].length-2)
+  }
+
+  cartItems = JSON.stringify(cartItems);
+  console.log(cartItems);
+  cartItems = JSON.parse(cartItems)
+  var totalPrice = 0;
+  console.log(cartItems);
+
+  for(var i = 0; i < cartItems.length; i++){
+    totalPrice = totalPrice + cartItems[i].price;
+    var div = ``;
+
+    div = div + `
+
+    <div class="cart-product">    
+    
+          <div class="product-image">
+            <img src="${cartItems[i].imgUrl}">
+          </div>
+          <div class="product-details">
+            <div class="product-title">${cartItems[i].name}</div>
+            <div class="product-title">${cartItems[i].skuId}</div>
+            <p class="product-description">${cartItems[i].description}</p>
+          </div>
+          <div class="product-price">${cartItems[i].price}</div>
+          <div class="product-removal">
+            <button class="remove-product" onclick="removeItem(id)">
+              Remove
+            </button>
+          </div>
+        </div>
+        
+    
+    `
+
+  }
+
+cart.innerHTML = div;
+  cart.style.display = "block";
+}
+
+
+loadCartNumber();
+

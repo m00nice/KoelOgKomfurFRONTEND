07ef9ae307af8c159b617caab895a7043978c347
gnimas <csamsinges@gmail.com>
2022-12-13 02:21:39 +0100

    hjælp

--- js/GetpriceTest.js
+++ js/GetpriceTest.js
@@ -2,6 +2,39 @@ var objectString = localStorage.getItem('user');
 var user = JSON.parse(objectString);
 var finaluser = JSON.parse(user);
 var discount = finaluser.ilveDiscount;
-
-
 console.log(discount);
+
+
+
+
+ class GetpriceTest{
+    
+
+    endpointUrlProduct = "http://localhost:8080/product";
+    
+    constructor(dataBrands){
+      
+        this.dataBrands = dataBrands;
+        this.fetchData();
+        console.log("constructor");
+    }
+    
+    fetchData(){
+        fetch("http://localhost:8080/product")
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    }
+    price(){
+        var data = this.dataBrands;
+        console.log(data.price)
+    }
+  
+
+
+
+
+    
+    
+
+ }
+ var generateProducts = new GetpriceTest();
